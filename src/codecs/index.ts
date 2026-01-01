b19d43c795b419651a2e70e72cc822ce79958187
DepsCian <deps2ak@gmail.com>
2026-01-01 22:06:24 +0200

    refactor: extract core, codecs and common modules

---
+++ src/codecs/index.ts
@@ -0,0 +1,5 @@
+export * from "./bitmap";
+export * from "./color";
+export * from "./dxt";
+export * from "./formats";
+export * from "./palette";
---
+++ src/core/index.ts
@@ -0,0 +1,5 @@
+export * from "./byte-stream";
+export * from "./rw-file";
+export * from "./rw-sections";
+export * from "./rw-version";
+export * from "./errors";
